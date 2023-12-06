3c2b7f3a0a35a9b1e417f1656cfaba79ad4e4722
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-12-06 09:39:32 +0100

    Day 6 part 2

--- AdventOfCode.Tests/Day6Tests.fs
+++ AdventOfCode.Tests/Day6Tests.fs
@@ -1,5 +1,6 @@
 module ``day 6 tests``
 
+open System
 open AdventOfCode.Day6
 open Xunit
 open FsUnit.Xunit
@@ -10,15 +11,15 @@ Distance:  9  40  200"
 [<Fact>]
 let ``Should parse input to race`` () =
     parseInputToLegs sampleInput
-    |> should equal [ { Time = 7; Record = 9 }
-                      { Time = 15; Record = 40 }
-                      { Time = 30; Record = 200 } ]
+    |> should equal [ { Time = 7L; Record = 9L }
+                      { Time = 15L; Record = 40L }
+                      { Time = 30L; Record = 200L } ]
     
 [<Theory>]
-[<InlineData( 7, 9, 4)>]
-[<InlineData( 15, 40, 8)>]
-[<InlineData( 30, 200, 9)>]
-let ``Should count leg options`` (time: int) (record: int) (expectedCount: int) =
+[<InlineData( 7L, 9L, 4L)>]
+[<InlineData( 15L, 40L, 8L)>]
+[<InlineData( 30L, 200L, 9L)>]
+let ``Should count leg options`` (time: Int64) (record: Int64) (expectedCount: Int64) =
     countLegOptions { Time = time; Record = record }
     |> should equal expectedCount
     
@@ -26,8 +27,8 @@ let ``Should count leg options`` (time: int) (record: int) (expectedCount: int) 
 let ``Should calculate options for race from sample input`` () =
     parseInputToLegs sampleInput
     |> List.map countLegOptions
-    |> List.fold (fun acc x -> acc * x) 1
-    |> should equal 288
+    |> List.fold (fun acc x -> acc * x) 1L
+    |> should equal 288L
     
 let dayInput = @"Time:        55     99     97     93
 Distance:   401   1485   2274   1405"
@@ -36,6 +37,13 @@ Distance:   401   1485   2274   1405"
 let ``Should calculate options for race from day input`` () =
     parseInputToLegs dayInput
     |> List.map countLegOptions
-    |> List.fold (fun acc x -> acc * x) 1
-    |> should equal 2374848
+    |> List.fold (fun acc x -> acc * x) 1L
+    |> should equal 2374848L
     
+[<Fact>]
+let ``Should calculate options for new race from input`` () =
+    countLegOptions { Time = 71530L; Record = 940200L }
+    |> should equal 71503L
+    
+    countLegOptions { Time = 55999793L; Record = 401148522741405L }
+    |> should equal 39132886L
--- AdventOfCode/Day6.fs
+++ AdventOfCode/Day6.fs
@@ -2,15 +2,15 @@ module AdventOfCode.Day6
 
 open System
 
-type Leg = { Time: int; Record: int }
+type Leg = { Time: Int64; Record: Int64 }
 
 let parseInputToLegs (input: string) : Leg list =
-    let takeNumbersFromLine (line: string): int list =
+    let takeNumbersFromLine (line: string): Int64 list =
         line.Substring(line.IndexOf(":") + 1).Split(" ", StringSplitOptions.RemoveEmptyEntries)
-        |> Array.map int
+        |> Array.map Int64.Parse
         |> Array.toList
     
-    let produceLegsFromNumbers (times: int list) (records: int list): Leg list =
+    let produceLegsFromNumbers (times: Int64 list) (records: Int64 list): Leg list =
         [for i in [0..times.Length-1] do
              yield { Time = times[i]; Record = records[i] } ]
     
@@ -19,15 +19,18 @@ let parseInputToLegs (input: string) : Leg list =
     
     produceLegsFromNumbers numbers[0] numbers[1]
  
-let calculateDistance (seconds: int) (remainingTime: int): int =
+let calculateDistance (seconds: Int64) (remainingTime: Int64): Int64 =
     seconds * remainingTime
     
-let countLegOptions (leg: Leg) : int =
-    [for i in [1..leg.Time-1] do
-        let distance = calculateDistance i (leg.Time - i)
-        if distance > leg.Record then
-            yield 1
-        else
-            yield 0
-        ]
-    |> List.sum
+let countLegOptions (leg: Leg) : Int64 =
+    let time = leg.Time - 1L
+    let mutable idx = 1L
+    let mutable sum = 0L
+    
+    while idx < time do
+        if (calculateDistance idx (leg.Time - idx)) > leg.Record then
+            sum <- sum + 1L
+        
+        idx <- idx + 1L
+    
+    sum
