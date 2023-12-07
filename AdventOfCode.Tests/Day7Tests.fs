e121242edf49de2fdd2edcd379e24f025bdeaef1
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-12-07 21:31:53 +0100

    Day 7

--- AdventOfCode.Tests/Day7Tests.fs
+++ AdventOfCode.Tests/Day7Tests.fs
@@ -1,7 +1,104 @@
 module ``day 7 tests``
 
 open System
+open System.IO
 open AdventOfCode.Day7
+open Microsoft.FSharp.Collections
 open Xunit
 open FsUnit.Xunit
 
+let sampleInput = @"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"
+
+[<Fact>]
+let ``Should parse sample hand with bid`` () =
+    parseHandWithBid "32T3K 765"
+    |> should equal { Bid = 765
+                      Cards = [ 3; 2; 10; 3; 13 ] }
+    
+    parseHandWithBid "T55J5 684"
+    |> should equal { Bid = 684
+                      Cards = [ 10; 5; 5; 1; 5 ] }
+    
+    parseHandWithBid "KK677 28"
+    |> should equal { Bid = 28
+                      Cards = [ 13; 13; 6; 7; 7 ] }
+    
+    parseHandWithBid "KTJJT 220"
+    |> should equal { Bid = 220
+                      Cards = [ 13; 10; 1; 1; 10 ] }
+    
+    parseHandWithBid "QQQJA 483"
+    |> should equal { Bid = 483
+                      Cards = [ 12; 12; 12; 1; 14 ] }
+
+let sampleCards : obj[] list =
+    [
+        [| [ 3; 2; 10; 3; 13 ]; HandType.OnePair |]
+        [| [ 10; 5; 5; 11; 5 ]; HandType.ThreeOfKind |]
+        [| [ 13; 13; 6; 7; 7 ]; HandType.TwoPair |]
+        [| [ 13; 10; 11; 11; 10 ]; HandType.TwoPair |]
+        [| [ 12; 12; 12; 11; 14 ]; HandType.ThreeOfKind |]
+        [| [ 12; 12; 12; 11; 11 ]; HandType.FullHouse |]
+    ]
+    
+[<Theory>]
+[<MemberData(nameof(sampleCards))>]
+let ``Should calculate hand type`` (cards: int list) (expectedType: HandType) =
+    calculateHandType cards
+    |> should equal expectedType
+    
+[<Fact>]
+let ``Should calculate for sample`` () =
+    sampleInput.Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.toList
+    |> List.map parseHandWithBid
+    |> calculateWinningsForHands
+    |> should equal 6440
+
+[<Fact>]
+let ``Should calculate for sample with joker`` () =
+    sampleInput.Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.toList
+    |> List.map parseHandWithBid
+    |> calculateWinningsForHandsWithJoker
+    |> should equal 5905
+    
+[<Fact>]
+let ``Should calculate for input with jokers`` () =
+    File.ReadAllText("./Inputs/Day7/input.txt").Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.toList
+    |> List.map parseHandWithBid
+    |> calculateWinningsForHandsWithJoker
+    |> should equal 250665248
+
+let betterInput = @"2345A 1
+Q2KJJ 13
+Q2Q2Q 19
+T3T3J 17
+T3Q33 11
+2345J 3
+J345A 2
+32T3K 5
+T55J5 29
+KK677 7
+KTJJT 34
+QQQJA 31
+JJJJJ 37
+JAAAA 43
+AAAAJ 59
+AAAAA 61
+2AAAA 23
+2JJJJ 53
+JJJJ2 41"
+
+[<Fact>]
+let ``Should calculate for sample with joker better input`` () =
+    betterInput.Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.toList
+    |> List.map parseHandWithBid
+    |> calculateWinningsForHandsWithJoker
+    |> should equal 6839
--- AdventOfCode/Day7.fs
+++ AdventOfCode/Day7.fs
@@ -1,2 +1,129 @@
 module AdventOfCode.Day7
 
+open System
+
+type HandType =
+    | FiveOfKind = 1
+    | FourOfKind = 2
+    | FullHouse = 3
+    | ThreeOfKind = 4
+    | TwoPair = 5
+    | OnePair = 6
+    | HighCard = 7
+
+type Hand = { Cards: int list; Bid: int }
+type HandWithType = { Hand: Hand; Type: HandType}
+
+let parseHandWithBid (input: string) : Hand =
+    let parseCardInput (input: string) : int =
+        match input with
+        | "A" -> 14
+        | "K" -> 13
+        | "Q" -> 12
+        //| "J" -> 11 
+        | "T" -> 10
+        | "9" -> 9
+        | "8" -> 8
+        | "7" -> 7
+        | "6" -> 6
+        | "5" -> 5
+        | "4" -> 4
+        | "3" -> 3
+        | "2" -> 2
+        | "J" -> 1
+        | _ -> failwith $"Unrecognized card: %s{input}"
+        
+    let parseHand (input: string) : int list =
+        [for i in [0..4] do
+            yield parseCardInput (string input[i]) ]
+    
+    let parts = input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    { Cards = parseHand parts[0]; Bid = Int32.Parse parts[1]; }
+    
+let calculateHandType (hand: int list): HandType =
+    
+    let groupedAndSortedNumbers (ranks: int list): (int * int) list =
+        ranks
+        |> List.groupBy id
+        |> List.map (fun (key, values) -> key, List.length values)
+        |> List.sortByDescending snd
+    
+    let groups = groupedAndSortedNumbers hand
+    let highestGroup = groups[0]
+    match highestGroup with
+    | _, 5 -> HandType.FiveOfKind
+    | _, 4 -> HandType.FourOfKind
+    | _, 3 -> if snd groups[1]= 2 then
+                  HandType.FullHouse
+              else
+                  HandType.ThreeOfKind
+    | _, 2 -> if snd groups[1] = 2 then
+                HandType.TwoPair
+              else
+                HandType.OnePair
+    | _, 1 -> HandType.HighCard
+    | _, _ -> failwith "something went wrong"
+    
+let applyJoker (baseType: HandType) (hand: int list) : HandType =
+    let apply (old: HandType) =
+        match old with
+        | HandType.FiveOfKind -> HandType.FiveOfKind
+        | HandType.FourOfKind -> HandType.FiveOfKind
+        | HandType.FullHouse -> HandType.FourOfKind
+        | HandType.ThreeOfKind -> HandType.FourOfKind
+        | HandType.TwoPair -> HandType.FullHouse
+        | HandType.OnePair -> HandType.ThreeOfKind
+        | HandType.HighCard -> HandType.OnePair
+        | _ -> ArgumentOutOfRangeException() |> raise
+
+    let rec applyMultipleTimes oldType remainingJokers =
+        match remainingJokers with
+        | 0 -> oldType
+        | _ -> applyMultipleTimes (apply oldType) (remainingJokers - 1)
+    
+    let reduceJokers (old: HandType) (jokers: int) : int =
+        if old = HandType.OnePair && jokers = 2 then
+            1
+        else
+            jokers
+    
+    let numberOfJokers = hand |> List.filter (fun x -> x = 1) |> List.length |> reduceJokers baseType
+    
+    let newType = applyMultipleTimes baseType numberOfJokers
+    newType
+
+let addTypeToHand (hand: Hand) : HandWithType =
+    { Hand = hand; Type = calculateHandType hand.Cards }
+    
+let addTypeToHandWithJokers (hand: Hand) : HandWithType =
+    let baseType = calculateHandType hand.Cards
+    { Hand = hand; Type = applyJoker baseType hand.Cards }
+ 
+let compareHands (hwt1: HandWithType) (hwt2: HandWithType) =
+    match hwt1.Type, hwt2.Type with
+    | t1, t2 when t1 <> t2 -> compare t1 t2
+    | _ ->
+        let compareCards card1 card2 =
+            compare card2 card1
+        List.compareWith compareCards hwt1.Hand.Cards hwt2.Hand.Cards
+    
+let sortHandType (handsWithType: HandWithType list): HandWithType list =
+    handsWithType
+    |> List.sortWith compareHands
+    |> List.rev
+
+let calculateWinningsForHands (hands: Hand list) : int =
+    hands
+    |> List.map addTypeToHand
+    |> sortHandType
+    |> List.map (fun h -> h.Hand.Bid)
+    |> List.mapi (fun index value -> (index + 1) * value)
+    |> List.sum
+
+let calculateWinningsForHandsWithJoker (hands: Hand list) : int =
+    hands
+    |> List.map addTypeToHandWithJokers
+    |> sortHandType
+    |> List.map (fun h -> h.Hand.Bid)
+    |> List.mapi (fun index value -> (index + 1) * value)
+    |> List.sum
