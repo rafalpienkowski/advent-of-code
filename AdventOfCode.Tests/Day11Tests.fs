086fbc8c2362af218acd997c225d0d779430e9dd
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-12-11 22:26:28 +0100

    Day 11 part 2 + refactor

--- AdventOfCode.Tests/Day11Tests.fs
+++ AdventOfCode.Tests/Day11Tests.fs
@@ -1,5 +1,6 @@
 module ``day 11 tests``
 
+open System
 open System.IO
 open AdventOfCode.Day11
 open Xunit
@@ -18,80 +19,46 @@ let sampleInput =
 #...#....."
 
 [<Fact>]
-let ``Should add expand galactic`` () =
-
-    let expectedGalactic =
-        @"....#........
-.........#...
-#............
-.............
-.............
-........#....
-.#...........
-............#
-.............
-.............
-.........#...
-#....#......."
-
-    sampleInput |> expandGalactic |> should equal expectedGalactic
-
-
-let sampleGalactic =
-    [ { Id = 1; X = 0; Y = 4 }
-      { Id = 2; X = 1; Y = 9 }
-      { Id = 3; X = 2; Y = 0 }
-      { Id = 4; X = 5; Y = 8 }
-      { Id = 5; X = 6; Y = 1 }
-      { Id = 6; X = 7; Y = 12 }
-      { Id = 7; X = 10; Y = 9 }
-      { Id = 8; X = 11; Y = 0 }
-      { Id = 9; X = 11; Y = 5 } ]
+let ``Should find shortest path for pair 2`` () =
+    let expansions = sampleInput |> countExpansions 2
     
-[<Fact>]
-let ``Should find every galactic`` () =
+    findShortestPath2 { Id = 1; X = 0; Y = 3 } { Id = 7; X = 8; Y = 7 }  expansions
+    |> should equal 15L
+    
+    findShortestPath2 { Id = 5; X = 5; Y = 1 } { Id = 9; X = 9; Y = 4 } expansions
+    |> should equal 9L
+    
+    findShortestPath2 { Id = 3; X = 2; Y = 0 } { Id = 6; X = 6; Y = 9 }  expansions
+    |> should equal 17L
+    
+    findShortestPath2 { Id = 8; X = 9; Y = 0 } { Id = 9; X = 9; Y = 4 } expansions
+    |> should equal 5L
+
+[<Theory>]
+[<InlineData(2, 374L)>]
+[<InlineData(10, 1030L)>]
+[<InlineData(100, 8410L)>]
+let ``Should find shortest paths for all galactics in sample with expansion 2`` (size: int) (expectedSumOfPaths: Int64) =
+    let expansions = sampleInput |> countExpansions size
+    
     sampleInput
-    |> expandGalactic
-    |> findEveryGalactic
-    |> should equal sampleGalactic
-
-[<Fact>]
-let ``Should find all pairs`` () =
-    sampleGalactic
-    |> findPairs
-    |> List.length
-    |> should equal 36
-    
-[<Fact>]
-let ``Should find shortest path for pair`` () =
-    findShortestPath { Id = 5; X = 6; Y = 1 } { Id = 9; X = 11; Y = 5 }
-    |> should equal 9
-    
-    findShortestPath { Id = 1; X = 0; Y = 4 } { Id = 7; X = 10; Y = 9 }
-    |> should equal 15
-    
-    findShortestPath { Id = 3; X = 2; Y = 0 } { Id = 6; X = 7; Y = 12 }
-    |> should equal 17
-    
-    findShortestPath { Id = 8; X = 11; Y = 0 } { Id = 9; X = 11; Y = 5 }
-    |> should equal 5
-
-[<Fact>]
-let ``Should find shortest paths for all galactics in sample`` () =
-    sampleInput
-    |> expandGalactic
     |> findEveryGalactic
     |> findPairs
-    |> List.map (fun galacticPair -> findShortestPath (fst galacticPair) (snd galacticPair) )
+    |> List.map (fun galacticPair -> findShortestPath2 (fst galacticPair) (snd galacticPair) expansions )
     |> List.sum
-    |> should equal 374
+    |> should equal expectedSumOfPaths
 
-[<Fact>]
-let ``Should find shortest paths for all galactics in input`` () =
-    File.ReadAllText("./Inputs/Day11/input.txt")
-    |> expandGalactic
+[<Theory>]
+[<InlineData(1_000_000, 10494813L)>]
+[<InlineData(2, 10494813L)>]
+let ``Should find shortest paths for all galactics in input 2`` (size: int) (expectedSumOfPaths: Int64) =
+    let input = File.ReadAllText("./Inputs/Day11/input.txt")
+    let expansions = input  |> countExpansions 2
+    
+    input
     |> findEveryGalactic
     |> findPairs
-    |> List.map (fun galacticPair -> findShortestPath (fst galacticPair) (snd galacticPair) )
+    |> List.map (fun galacticPair -> findShortestPath2 (fst galacticPair) (snd galacticPair) expansions)
     |> List.sum
-    |> should equal 374
+    |> should equal expectedSumOfPaths
+
--- AdventOfCode/Day11.fs
+++ AdventOfCode/Day11.fs
@@ -1,56 +1,66 @@
 module AdventOfCode.Day11
 
 open System
-open System.Text
 
-type Galactic = { X: int; Y: int; Id: int }
+type Galactic = { X: Int64; Y: Int64; Id: Int64 }
 
-let expandGalactic (input: string) : string =
-    let lines = input.Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
-    
-    let mutable newLines = []
-    for line in lines do
-        newLines <- newLines @ [ line ]
-        if line.ToCharArray() |> Array.forall (fun c -> c = '.') then
-            newLines <- newLines @ [ line ]
+type Expansions =
+    { X: int list
+      Y: int list
+      Size: Int64 }
 
-    let columns = newLines[0].Length
-    let mutable newColumns = []
-    for column in [0..columns-1] do
-        let testedColumn = newLines |> List.map (fun line -> line[column])
-        newColumns <- newColumns @ [ testedColumn ]
-        if testedColumn |> List.forall (fun c -> c = '.') then
-            newColumns <- newColumns @ [ testedColumn ]
-    
-    let newColumnsLength = newColumns.Length - 1
-    let newRowsLength = newColumns[0].Length - 1
-    let galacticBuilder = StringBuilder()
-    
-    for j in [0..newRowsLength] do
-        for i in [0..newColumnsLength] do
-            galacticBuilder.Append(newColumns[i][j]) |> ignore
-        galacticBuilder.AppendLine() |> ignore
-    
-    galacticBuilder.ToString().TrimEnd()
-    
-    
+let countExpansions (expansion: int) (galactic: string) : Expansions =
+
+    let countExpansionX (galactic: string[]) : int list =
+        [ for idx in 0 .. galactic.Length - 1 do
+              if galactic[idx].ToCharArray() |> Array.forall (fun c -> c = '.') then
+                  yield idx ]
+
+    let countExpansionY (galactic: string[]) : int list =
+        [ for idx in 0 .. galactic[0].Length - 1 do
+              let testedColumn = galactic |> Array.map (fun line -> line[idx])
+
+              if testedColumn |> Array.forall (fun c -> c = '.') then
+                  yield idx ]
+
+    let galacticLines =
+        galactic.Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
+
+    { X = countExpansionX galacticLines
+      Y = countExpansionY galacticLines
+      Size = int64 expansion - 1L }
+
 let findEveryGalactic (galactic: string) : Galactic list =
     let lines = galactic.Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
     let rows = lines.Length - 1
     let columns = lines[0].Length - 1
     let mutable id = 0
-    let galactic = [ for row in [0..rows] do
-                        for column in [0..columns] do
-                            if lines[row][column] = '#' then
-                                id <- id + 1
-                                { Id = id; X = row; Y = column }]
-    galactic
 
-let findPairs (galactics: Galactic list): (Galactic * Galactic) list =
+    [ for row in [ 0..rows ] do
+          for column in [ 0..columns ] do
+              if lines[row][column] = '#' then
+                  id <- id + 1
+                  { Id = id; X = row; Y = column } ]
+
+let findPairs (galactics: Galactic list) : (Galactic * Galactic) list =
     [ for galactic1 in galactics do
-        for galactic2 in galactics do
-            if galactic1.Id < galactic2.Id then
-                yield (galactic1, galactic2) ]
-    
-let findShortestPath (galactic1: Galactic) (galactic2: Galactic) : int =
-    Math.Abs(galactic2.X - galactic1.X) + Math.Abs(galactic2.Y - galactic1.Y)
+          for galactic2 in galactics do
+              if galactic1.Id < galactic2.Id then
+                  yield (galactic1, galactic2) ]
+
+let findShortestPath2 (galactic1: Galactic) (galactic2: Galactic) (expansions: Expansions) : Int64 =
+
+    let expandsX =
+        expansions.X
+        |> List.filter (fun e -> (galactic1.X < e && galactic2.X > e) || (galactic2.X < e && galactic1.X > e))
+        |> List.length
+
+    let expandsY =
+        expansions.Y
+        |> List.filter (fun e -> (galactic1.Y < e && galactic2.Y > e) || (galactic2.Y < e && galactic1.Y > e))
+        |> List.length
+
+    Math.Abs(galactic2.X - galactic1.X)
+    + (int64 expandsX * expansions.Size)
+    + Math.Abs(galactic2.Y - galactic1.Y)
+    + (int64 expandsY * expansions.Size)
