4b7e22dee52f6f88efd1f4b2dd363f149a88e472
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-12-15 14:28:52 +0100

    Day 15 part 1 completed

--- AdventOfCode.Tests/Day15Tests.fs
+++ AdventOfCode.Tests/Day15Tests.fs
@@ -1,6 +1,31 @@
 module ``day 15 tests``
 
+open System
 open System.IO
 open AdventOfCode.Day15
 open Xunit
 open FsUnit.Xunit
+
+let hashSample = "HASH"
+
+[<Fact>]
+let ``Should calculate hash for hash input`` () =
+    hashSample
+    |> hash
+    |> should equal 52
+
+let sampleInput = @"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+
+[<Fact>]
+let ``Should calculate hash for sample input`` () =
+    sampleInput.Split(',', StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map hash
+    |> Array.sum
+    |> should equal 1320
+    
+[<Fact>]
+let ``Should calculate hash for test input`` () =
+    File.ReadAllText("./Inputs/Day15/input.txt").Split(',', StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map hash
+    |> Array.sum
+    |> should equal 521341
--- AdventOfCode/Day15.fs
+++ AdventOfCode/Day15.fs
@@ -1,3 +1,14 @@
 module AdventOfCode.Day15
 
+let hash (input: string) : int =
     
+    let rec calculate (current: string) (sum: int): int =
+        if current.Length = 0 then
+            sum
+        else
+            let charValue = int current[0]
+            let newSum = ((sum + charValue) * 17) % 256
+            
+            calculate (current.Substring(1, current.Length - 1)) newSum
+    
+    calculate input 0
