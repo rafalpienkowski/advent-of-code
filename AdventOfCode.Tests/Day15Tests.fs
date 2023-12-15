221ca008c2ca4a30d3b4f72a60e4fe4dcb809763
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-12-15 21:18:12 +0100

    Day 15 part 2

--- AdventOfCode.Tests/Day15Tests.fs
+++ AdventOfCode.Tests/Day15Tests.fs
@@ -9,10 +9,7 @@ open FsUnit.Xunit
 let hashSample = "HASH"
 
 [<Fact>]
-let ``Should calculate hash for hash input`` () =
-    hashSample
-    |> hash
-    |> should equal 52
+let ``Should calculate hash for hash input`` () = hashSample |> hash |> should equal 52
 
 let sampleInput = @"rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 
@@ -22,10 +19,53 @@ let ``Should calculate hash for sample input`` () =
     |> Array.map hash
     |> Array.sum
     |> should equal 1320
-    
+
 [<Fact>]
 let ``Should calculate hash for test input`` () =
-    File.ReadAllText("./Inputs/Day15/input.txt").Split(',', StringSplitOptions.RemoveEmptyEntries)
+    File
+        .ReadAllText("./Inputs/Day15/input.txt")
+        .Split(',', StringSplitOptions.RemoveEmptyEntries)
     |> Array.map hash
     |> Array.sum
     |> should equal 521341
+
+
+[<Fact>]
+let ``Should load initialisation script from sample input`` () =
+    sampleInput
+    |> loadInitialisation
+    |> should
+        equal
+        [| { Lens = { Label = "rn"; FocalLength = 1 }
+             Action = Save }
+           { Lens = { Label = "cm"; FocalLength = 0 }
+             Action = Remove }
+           { Lens = { Label = "qp"; FocalLength = 3 }
+             Action = Save }
+           { Lens = { Label = "cm"; FocalLength = 2 }
+             Action = Save }
+           { Lens = { Label = "qp"; FocalLength = 0 }
+             Action = Remove }
+           { Lens = { Label = "pc"; FocalLength = 4 }
+             Action = Save }
+           { Lens = { Label = "ot"; FocalLength = 9 }
+             Action = Save }
+           { Lens = { Label = "ab"; FocalLength = 5 }
+             Action = Save }
+           { Lens = { Label = "pc"; FocalLength = 0 }
+             Action = Remove }
+           { Lens = { Label = "pc"; FocalLength = 6 }
+             Action = Save }
+           { Lens = { Label = "ot"; FocalLength = 7 }
+             Action = Save } |]
+
+[<Fact>]
+let ``Should calculate focus power for boxes from sample input`` () =
+    sampleInput |> initialise |> calculateFocusingPowerFor |> should equal 145
+
+[<Fact>]
+let ``Should calculate focus power for boxes from test input`` () =
+    File.ReadAllText("./Inputs/Day15/input.txt")
+    |> initialise
+    |> calculateFocusingPowerFor
+    |> should equal 252782
--- AdventOfCode/Day15.fs
+++ AdventOfCode/Day15.fs
@@ -1,14 +1,98 @@
 module AdventOfCode.Day15
 
+open System
+open Microsoft.FSharp.Collections
+
+type Lens = { Label: string; FocalLength: int }
+
+type Operation =
+    | Remove
+    | Save // Insert or Update
+
+type InitialisationStep = { Lens: Lens; Action: Operation }
+
+type Box = { Id: int; Lens: Lens seq }
+
 let hash (input: string) : int =
-    
-    let rec calculate (current: string) (sum: int): int =
+
+    let rec calculate (current: string) (sum: int) : int =
         if current.Length = 0 then
             sum
         else
             let charValue = int current[0]
             let newSum = ((sum + charValue) * 17) % 256
-            
+
             calculate (current.Substring(1, current.Length - 1)) newSum
-    
+
     calculate input 0
+
+let loadInitialisation (input: string) : InitialisationStep array =
+
+    let toStep (step: string) : InitialisationStep =
+        let removeIdx = step.IndexOf('-')
+        let saveIdx = step.IndexOf('=')
+        let signIdx = [| removeIdx; saveIdx |] |> Array.max
+
+        { Action = if removeIdx > 0 then Operation.Remove else Operation.Save
+          Lens =
+            { Label = step.Substring(0, signIdx)
+              FocalLength =
+                if removeIdx > 0 then
+                    0
+                else
+                    int (step.Substring(signIdx + 1)) } }
+
+    input.Split(',', StringSplitOptions.RemoveEmptyEntries) |> Array.map toStep
+
+
+let initialise (input: string) : Box array =
+    let steps = input |> loadInitialisation
+    let boxes = Array.init 256 (fun i -> { Id = i; Lens = Seq.empty })
+
+    let rec initialiseStep (boxes: Box array) (steps: InitialisationStep array) =
+        if steps.Length = 0 then
+            boxes
+        else
+            let newBoxes =
+                boxes
+                |> Array.mapi (fun i x ->
+                    if i = (steps[0].Lens.Label |> hash) then
+                        match steps[0].Action with
+                        | Remove ->
+                            { x with
+                                Lens = x.Lens |> Seq.where (fun l -> l.Label <> steps[0].Lens.Label) }
+                        | Save ->
+                            if x.Lens |> Seq.exists (fun l -> l.Label = steps[0].Lens.Label) then
+                                { x with
+                                    Lens =
+                                        x.Lens
+                                        |> Seq.map (fun l ->
+                                            if l.Label = steps[0].Lens.Label then steps[0].Lens else l) }
+                            else
+                                { x with
+                                    Lens = x.Lens |> Seq.append (steps[0].Lens |> Seq.singleton) }
+                    else
+                        x)
+
+            initialiseStep newBoxes (steps |> Array.tail)
+
+    initialiseStep boxes steps
+
+let calculateFocusingPowerFor (boxes: Box array) : int =
+
+    let rec calculateForBox (boxes: Box array) (currentValue: int) (idx: int) =
+
+        if boxes.Length = 0 then
+            currentValue
+        else
+            let newValue =
+                boxes[0].Lens
+                |> Seq.rev
+                |> Seq.fold
+                    (fun (acc, index) lens -> (acc + (idx * index * lens.FocalLength), index + 1))
+                    (currentValue, 1)
+                |> fst
+
+            calculateForBox (boxes |> Array.tail) newValue (idx + 1)
+
+    calculateForBox boxes 0 1
