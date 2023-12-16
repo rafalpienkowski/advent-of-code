4d9a716e08419d6c4e950aa6352b02b93e8eb1d7
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-12-16 19:40:52 +0100

    Day 16 part 1

---
+++ AdventOfCode.Tests/Day16Tests.fs
@@ -0,0 +1,266 @@
+module ``day 16 tests``
+
+open System
+open System.IO
+open AdventOfCode.Day16
+open Xunit
+open FsUnit.Xunit
+
+let sampleInput =
+    @".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."
+
+let expectedEnergizedContraption =
+    @"######....
+.#...#....
+.#...#####
+.#...##...
+.#...##...
+.#...##...
+.#..####..
+########..
+.#######..
+.#...#.#.."
+
+[<Fact>]
+let ``Should energize contraption from sample input`` () =
+    let contraption = sampleInput |> loadContraption
+    let energized = contraption |> energize |> normalize
+    dump contraption energized |> should equal expectedEnergizedContraption
+
+[<Fact>]
+let ``Should count energized contraption from sample input`` () =
+    let positions = sampleInput
+                    |> loadContraption
+                    |> energize
+                    |> normalize
+    positions.Length |> should equal 46
+    
+[<Fact>]
+let ``Should count energized contraption from test input`` () =
+    let positions = File.ReadAllText("./Inputs/Day16.txt")
+                    |> loadContraption
+                    |> energize
+                    |> normalize
+    positions.Length |> should equal 7060
+
+[<Fact>]
+let ``Should move in correct direction for '.'`` () =
+    let symbol = '.'
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Up }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 3; Y = 4 }
+            Direction = Up } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Down }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 3; Y = 6 }
+            Direction = Down } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Right }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 4; Y = 5 }
+            Direction = Right } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Left }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 2; Y = 5 }
+            Direction = Left } ]
+
+
+[<Fact>]
+let ``Should move in correct direction for '|'`` () =
+    let symbol = '|'
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Up }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 3; Y = 4 }
+            Direction = Up } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Down }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 3; Y = 6 }
+            Direction = Down } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Right }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 3; Y = 4 }
+            Direction = Up }
+          { Position = { X = 3; Y = 6 }
+            Direction = Down } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Left }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 3; Y = 4 }
+            Direction = Up }
+          { Position = { X = 3; Y = 6 }
+            Direction = Down } ]
+
+[<Fact>]
+let ``Should move in correct direction for '-'`` () =
+    let symbol = '-'
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Right }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 4; Y = 5 }
+            Direction = Right } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Left }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 2; Y = 5 }
+            Direction = Left } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Up }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 4; Y = 5 }
+            Direction = Right }
+          { Position = { X = 2; Y = 5 }
+            Direction = Left } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Down }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 4; Y = 5 }
+            Direction = Right }
+          { Position = { X = 2; Y = 5 }
+            Direction = Left } ]
+        
+[<Fact>]
+let ``Should move in correct direction for '/'`` () =
+    let symbol = '/'
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Right }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 3; Y = 4 }
+            Direction = Up } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Up }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 4; Y = 5 }
+            Direction = Right } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 4 }
+          Direction = Left }
+        symbol
+    |> should
+        equal
+        [ 
+          { Position = { X = 3; Y = 5 }
+            Direction = Down } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Down }
+        symbol
+    |> should
+        equal
+        [ 
+          { Position = { X = 2; Y = 5 }
+            Direction = Left } ]
+
+[<Fact>]
+let ``Should move in correct direction for '\'`` () =
+    let symbol = '\\'
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Right }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 3; Y = 6 }
+            Direction = Down } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Up }
+        symbol
+    |> should
+        equal
+        [ { Position = { X = 2; Y = 5 }
+            Direction = Left } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 4 }
+          Direction = Left }
+        symbol
+    |> should
+        equal
+        [ 
+          { Position = { X = 3; Y = 3 }
+            Direction = Up } ]
+
+    nextMovesFrom
+        { Position = { X = 3; Y = 5 }
+          Direction = Down }
+        symbol
+    |> should
+        equal
+        [ 
+          { Position = { X = 4; Y = 5 }
+            Direction = Right } ]
---
+++ AdventOfCode/Day16.fs
@@ -0,0 +1,180 @@
+module AdventOfCode.Day16
+
+open System
+open System.Text
+
+type Direction =
+    | Up
+    | Down
+    | Left
+    | Right
+
+type Position = { X: int; Y: int }
+
+type Move =
+    { Position: Position
+      Direction: Direction }
+
+let rows = Array2D.length1
+let columns = Array2D.length2
+
+let loadContraption (input: string) : char[,] =
+    let lines = input.Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
+    let numRows = Array.length lines
+    let numCols = lines |> Array.map (fun line -> line.Length) |> Array.max
+
+    Array2D.init numRows numCols (fun row col -> lines[col][row])
+
+let nextPosition (move: Move) : Position =
+    match move.Direction with
+    | Up ->
+        { move.Position with
+            Y = move.Position.Y - 1 }
+    | Down ->
+        { move.Position with
+            Y = move.Position.Y + 1 }
+    | Left ->
+        { move.Position with
+            X = move.Position.X - 1 }
+    | Right ->
+        { move.Position with
+            X = move.Position.X + 1 }
+
+let nextMovesFrom (move: Move) (symbol: char) : Move list =
+    match symbol with
+    | '.' ->
+        [ { Direction = move.Direction
+            Position = move |> nextPosition } ]
+    | '\\' ->
+        match move.Direction with
+        | Up ->
+            [ { Direction = Left
+                Position =
+                  { X = move.Position.X - 1
+                    Y = move.Position.Y } } ]
+        | Down ->
+            [ { Direction = Right
+                Position =
+                  { X = move.Position.X + 1
+                    Y = move.Position.Y } } ]
+        | Left ->
+            [ { Direction = Up
+                Position =
+                  { X = move.Position.X
+                    Y = move.Position.Y - 1 } } ]
+        | Right ->
+            [ { Direction = Down
+                Position =
+                  { X = move.Position.X
+                    Y = move.Position.Y + 1 } } ]
+    | '/' ->
+        match move.Direction with
+        | Up ->
+            [ { Direction = Right
+                Position =
+                  { X = move.Position.X + 1
+                    Y = move.Position.Y } } ]
+        | Down ->
+            [ { Direction = Left
+                Position =
+                  { X = move.Position.X - 1
+                    Y = move.Position.Y } } ]
+        | Left ->
+            [ { Direction = Down
+                Position =
+                  { X = move.Position.X
+                    Y = move.Position.Y + 1 } } ]
+        | Right ->
+            [ { Direction = Up
+                Position =
+                  { X = move.Position.X
+                    Y = move.Position.Y - 1 } } ]
+    | '-' ->
+        if move.Direction = Right || move.Direction = Left then
+            [ { Direction = move.Direction
+                Position = move |> nextPosition } ]
+        else
+            [ { Direction = Right
+                Position =
+                  { X = move.Position.X + 1
+                    Y = move.Position.Y } }
+              { Direction = Left
+                Position =
+                  { X = move.Position.X - 1
+                    Y = move.Position.Y } } ]
+    | '|' ->
+        if move.Direction = Up || move.Direction = Down then
+            [ { Direction = move.Direction
+                Position = move |> nextPosition } ]
+        else
+            [ { Direction = Up
+                Position =
+                  { X = move.Position.X
+                    Y = move.Position.Y - 1 } }
+              { Direction = Down
+                Position =
+                  { X = move.Position.X
+                    Y = move.Position.Y + 1 } } ]
+    | _ -> failwith "Unrecognized element"
+
+let energize (contraption: char[,]) : Map<Move, char> =
+    let energized = Map.empty
+    let initialPosition = { X = 0; Y = 0 }
+
+    let isInContraption (position: Position) : bool =
+        position.X >= 0
+        && position.Y >= 0
+        && position.X < (contraption |> rows)
+        && position.Y < (contraption |> columns)
+
+    let tailIfMultipleElements lst =
+        match lst with
+        | [] -> [] // Empty list
+        | _ :: rest -> rest
+
+    let rec light (currentMoves: Move list) (currentEnergized: Map<Move, char>) : Map<Move, char> =
+        if currentMoves.Length = 0 then
+            currentEnergized
+        else
+            let currentMove = currentMoves[0]
+
+            if
+                not (currentMove.Position |> isInContraption)
+                || (currentEnergized |> Map.containsKey currentMove)
+            then
+                light (currentMoves |> tailIfMultipleElements) currentEnergized
+            else
+                let newEnergized = currentEnergized |> Map.add currentMove '#'
+                let currentSymbol = contraption[currentMove.Position.X, currentMove.Position.Y]
+
+                let nextMoves =
+                    (currentMoves |> tailIfMultipleElements)
+                    @ (nextMovesFrom currentMove currentSymbol)
+
+                light nextMoves newEnergized
+
+    let initialMoves =
+        [ { Position = initialPosition
+            Direction = Right } ]
+
+    light initialMoves energized
+
+let normalize (energized: Map<Move, char>) : Position list =
+    energized.Keys
+    |> Seq.map (fun move -> move.Position)
+    |> Seq.distinct
+    |> List.ofSeq
+
+let dump (contraption: char[,]) (energized: Position list) : string =
+    let contraptionBuilder = StringBuilder()
+
+    for row in [ 0 .. (contraption |> rows) - 1 ] do
+        for column in [ 0 .. (contraption |> columns) - 1 ] do
+            if energized |> List.contains ({ X = column; Y = row }) then
+                contraptionBuilder.Append('#') |> ignore
+            else
+                contraptionBuilder.Append('.') |> ignore
+
+        contraptionBuilder.AppendLine() |> ignore
+
+    contraptionBuilder.ToString().TrimEnd()
