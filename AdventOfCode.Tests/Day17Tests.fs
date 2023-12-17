5756680bc4145ef21c6796520b3d2db44f1dfe72
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2023-12-17 20:53:54 +0100

    WIP

--- AdventOfCode.Tests/Day17Tests.fs
+++ AdventOfCode.Tests/Day17Tests.fs
@@ -5,3 +5,29 @@ open AdventOfCode.Day17
 open Xunit
 open FsUnit.Xunit
 
+let sampleInput = @"2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"
+
+let simplifiedInput = @"24134
+32154
+32552
+34465
+45466"
+
+[<Fact>]
+let ``Should calculate lost for simplified map`` () =
+    simplifiedInput
+    |> loadCrucibles
+    |> pathHeatLost
+    |> should equal 6
--- AdventOfCode/Day17.fs
+++ AdventOfCode/Day17.fs
@@ -1,2 +1,64 @@
 module AdventOfCode.Day17
 
+open System
+open System.Text
+
+type Position = { X: int; Y: int }
+type Analysis = { Lost: int; Visited: bool }
+
+type Crucibles = Map<Position, int>
+type CruciblesAnalysis = Map<Position, Analysis>
+
+let rows = Array2D.length1
+let columns = Array2D.length2
+
+let loadCrucibles (input: string) : Crucibles =
+    let lines = input.Split([| "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
+    let numRows = Array.length lines
+    let numCols = lines |> Array.map (fun line -> line.Length) |> Array.max
+    let crucibles = Map.empty
+
+    for row in [0 .. (numRows - 1)] do
+        for col in [0 .. (numCols - 1)] do
+            if row = 0 && col = 0 then
+                crucibles |> Map.add ({ X = row; Y = col }, 0) |> ignore
+            else
+                let lost = int lines[col].[row]
+                crucibles |> Map.add ({ X = row; Y = col }, lost) |> ignore
+                
+    crucibles
+
+let getNeighborsFor (position: Position) : Position list =
+    []
+
+let pathHeatLost (crucibles: Crucibles) : int =
+    let analysis = crucibles
+                    |> Map.toSeq
+                    |> Seq.map ( fun (key, _) -> key, { Lost = Int32.MaxValue; Visited = false })
+                    |> Map.ofSeq
+    let initial = { X = 0; Y = 0 }
+    
+    let rec findPath (positionsToCheck: Position list) (analysis: CruciblesAnalysis) =
+        if positionsToCheck.Length = 0 then
+            0
+        else
+            let currentPosition = positionsToCheck[0]
+            let newPositionsToCheck = positionsToCheck |> List.tail
+            findPath newPositionsToCheck analysis
+    
+    findPath [ initial ] analysis
+    
+    
+let dump (crucibles: int[,]) (path: Position list) : string =
+    let contraptionBuilder = StringBuilder()
+
+    for row in [ 0 .. (crucibles |> rows) - 1 ] do
+        for column in [ 0 .. (crucibles |> columns) - 1 ] do
+            if path |> List.contains { X = column; Y = row } then
+                contraptionBuilder.Append('#') |> ignore
+            else
+                contraptionBuilder.Append('.') |> ignore
+
+        contraptionBuilder.AppendLine() |> ignore
+
+    contraptionBuilder.ToString().TrimEnd()
