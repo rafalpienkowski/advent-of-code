4f9eca7fa2beb3b53ea2524513f8e2c59a7229b0
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2024-12-18 20:34:42 +0100

    Day 16

--- 2024/days/16_test.go
+++ 2024/days/16_test.go
@@ -30,7 +30,7 @@ func getDataDay16() [][]rune {
 }
 
 type MazeStep struct {
-	From, To          Point
+	From, To        Point
 	Direction, Dist int
 }
 
@@ -39,9 +39,9 @@ func adj(pd PointDir, maze [][]rune) []MazeStep {
 
 	var dirs = [4]Point{
 		{X: 1, Y: 0},
-		{X: 0, Y: -1},
-		{X: -1, Y: 0},
 		{X: 0, Y: 1},
+		{X: -1, Y: 0},
+		{X: 0, Y: -1},
 	}
 
 	dir := dirs[pd.D]
@@ -54,14 +54,14 @@ func adj(pd PointDir, maze [][]rune) []MazeStep {
 	dir = dirs[(pd.D+1)%4]
 	next = pd.P.Add(dir)
 	if maze[next.Y][next.X] != '#' {
-		nextStep := MazeStep{From: pd.P, To: next, Direction: (pd.D + 1) % 4, Dist: 1000}
+		nextStep := MazeStep{From: pd.P, To: next, Direction: (pd.D + 1) % 4, Dist: 1001}
 		adjs = append(adjs, nextStep)
 	}
 
 	dir = dirs[(pd.D+3)%4]
 	next = pd.P.Add(dir)
 	if maze[next.Y][next.X] != '#' {
-		nextStep := MazeStep{From: pd.P, To: next, Direction: (pd.D + 3) % 4, Dist: 1000}
+		nextStep := MazeStep{From: pd.P, To: next, Direction: (pd.D + 3) % 4, Dist: 1001}
 		adjs = append(adjs, nextStep)
 	}
 
@@ -74,13 +74,45 @@ type PointDir struct {
 	Dist int
 }
 
-func findMinPath(maze [][]rune) int {
+type PointD struct {
+	P Point
+	D int
+}
+
+type Item struct {
+	node     Point
+	distance int
+	index    int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int           { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool { return pq[i].distance < pq[j].distance }
+
+func (pq PriorityQueue) Swap(
+	i, j int,
+) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+func (pq *PriorityQueue) Push(x interface{}) { *pq = append(*pq, x.(*Item)) }
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	*pq = old[:n-1]
+	return item
+}
+
+func findMinPath(maze [][]rune) (int, int) {
 	var start, end Point
 	dist := make(map[Point]int)
-	path := make(map[Point]Point)
+	path := make(map[Point][]Point)
 	var queue []PointDir
 	defPoint := Point{X: -1, Y: -1}
-
 	for y := range maze {
 		for x := range maze[y] {
 			if maze[y][x] == '#' {
@@ -88,7 +120,6 @@ func findMinPath(maze [][]rune) int {
 			}
 
 			p := Point{X: x, Y: y}
-			path[p] = defPoint
 			dist[p] = math.MaxInt
 
 			if maze[y][x] == 'S' {
@@ -103,39 +134,69 @@ func findMinPath(maze [][]rune) int {
 		}
 	}
 
-    queue = append(queue, PointDir{P: start, D: 0, Dist: 0})
+	paths := make(map[Point][][]Point)
+	paths[start] = [][]Point{{start}}
 
-	idx := 0
+	queue = append(queue, PointDir{P: start, D: 0, Dist: 0})
 
-	//for len(queue) > 0 {
-	for idx < 15 {
-        if len(queue) == 0 {
-            fmt.Printf("End \n")
-            break
-        }
-		fmt.Println("-----------------------")
-		idx++
-		for _, v := range queue {
-			fmt.Printf("%v\n", v)
+	findMin := func() PointDir {
+		tmp := PointDir{P: defPoint, D: 0, Dist: math.MaxInt}
+		for _, pd := range queue {
+			if pd.Dist < tmp.Dist {
+				tmp = pd
+			}
 		}
 
-		curr := queue[0]
-		queue = queue[1:]
-		if curr.P == end {
-			fmt.Printf("Found!!!")
-            fmt.Printf("Steps: %v", idx)
-			return curr.Dist
+		return tmp
+	}
+
+	remove := func(pd PointDir) []PointDir {
+		var tmp []PointDir
+
+		for _, p := range queue {
+			if p == pd {
+				continue
+			}
+
+			tmp = append(tmp, p)
 		}
+
+		return tmp
+	}
+
+	for len(queue) > 0 {
+		curr := findMin()
+		queue = remove(curr)
 
 		for _, a := range adj(curr, maze) {
-			if curr.Dist + a.Dist < dist[a.To] {
-                dist[a.To] = curr.Dist + a.Dist
-                queue = append(queue, PointDir{P: a.To, D: a.Direction, Dist: dist[a.To]})
+
+			if dist[a.From]+a.Dist < dist[a.To] {
+				dist[a.To] = dist[a.From] + a.Dist
+				queue = append(queue, PointDir{P: a.To, D: a.Direction, Dist: dist[a.To]})
+
+				paths[a.To] = [][]Point{}
+				for _, pat := range paths[curr.P] {
+					newPath := append([]Point{}, pat...)
+					paths[a.To] = append(paths[a.To], append(newPath, a.To))
+				}
+
+				path[a.To] = append(path[a.To], a.From)
+			} else if dist[a.From]+a.Dist == dist[a.To] {
+				path[a.To] = append(path[a.To], a.From)
+
+				for _, pat := range paths[curr.P] {
+					newPath := append([]Point{}, pat...)
+					paths[a.To] = append(paths[a.To], append(newPath, a.To))
+				}
 			}
 		}
 	}
 
-	return dist[end]
+	fmt.Printf("Paths: %v\n", paths[end])
+
+	res := make(map[Point]bool)
+
+	return dist[end], len(res)
 }
 
 func printMaze(maze [][]rune) {
@@ -145,10 +206,12 @@ func printMaze(maze [][]rune) {
 	fmt.Println()
 }
 
-func Day_16(t *testing.T) {
+func Test_Day_16(t *testing.T) {
 	maze := getDataDay16()
 	printMaze(maze)
-	result1 := findMinPath(maze)
+	result1, result2 := findMinPath(maze)
 
 	assert.EqualValues(t, 7036, result1)
+	assert.EqualValues(t, 45, result2)
+	//assert.EqualValues(t, 130536, result1)
 }
