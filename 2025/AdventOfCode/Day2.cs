49a013683b835f3c44c44a1ce4c93890b2748ed3
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2025-12-02 16:56:11 +0100

    Day 2 part 2

--- 2025/AdventOfCode/Day2.cs
+++ 2025/AdventOfCode/Day2.cs
@@ -16,12 +16,71 @@ class Finder
 
             for (long i = start; i <= end; i++)
             {
-                if (IsRepeatedDigit(i))
+                if (IsRepeatedAtLeastTwice(i))
                 {
+                    Console.WriteLine($"Adding {i} to sum");
                     Sum += i;
                 }
             }
         }
+    }
+
+    private bool IsRepeatedAtLeastTwice(long number)
+    {
+        var str = number.ToString();
+        var length = str.Length;
+
+        for (int size = 1; size <= length / 2; size++)
+        {
+            int start = 0;
+            var first = str.Substring(start, size);
+            if (first == "0")
+                continue;
+
+            var second = str.Substring(start + size, size);
+
+            if (first == second)
+            {
+                if (first.Length + second.Length < length)
+                {
+
+                    var leftover = str.Substring(first.Length + second.Length);
+                    if (IsRepeated(first, leftover))
+                    {
+                        Console.WriteLine($"Found repeated sequence {first} in number {number}");
+                        return true;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"Found repeated sequence {first} in number {number}");
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private bool IsRepeated(string first, string leftover)
+    {
+        if (leftover.Substring(0, Math.Min(first.Length, leftover.Length)) == first)
+        {
+            if (first.Length == leftover.Length)
+            {
+                Console.WriteLine($"Found repeated sequence {first}, leftover {leftover} (full match)");
+                return true;
+            }
+            else
+            {
+                if (IsRepeated(first, leftover.Substring(first.Length)))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private bool IsRepeatedDigit(long number)
@@ -40,7 +99,7 @@ class Finder
 
 public class Day2
 {
-    [Fact]
+    //[Fact]
     public void Test1()
     {
         var lines = AdventOfCode.Utils.ReadInputLines("day2.txt");
@@ -51,7 +110,7 @@ public class Day2
         Assert.Equal(1227775554, finder.Sum);
     }
 
-    [Fact]
+    //[Fact]
     public void Test2()
     {
         var lines = AdventOfCode.Utils.ReadInputLines("day2b.txt");
@@ -60,5 +119,27 @@ public class Day2
         finder.Load(line);
 
         Assert.Equal(37314786486, finder.Sum);
+    }
+
+    //[Fact]
+    public void Test3()
+    {
+        var lines = AdventOfCode.Utils.ReadInputLines("day2.txt");
+        var line = lines.First();
+        var finder = new Finder();
+        finder.Load(line);
+
+        Assert.Equal(4174379265, finder.Sum);
+    }
+
+    //[Fact]
+    public void Test4()
+    {
+        var lines = AdventOfCode.Utils.ReadInputLines("day2b.txt");
+        var line = lines.First();
+        var finder = new Finder();
+        finder.Load(line);
+
+        Assert.Equal(4174379265, finder.Sum);
     }
 }
