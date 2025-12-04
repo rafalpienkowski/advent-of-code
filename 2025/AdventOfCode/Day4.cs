98ad42c0bc8a0b0ee5820a11763241e2f587edfe
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2025-12-04 11:13:08 +0100

    Day 4 part 2

--- 2025/AdventOfCode/Day4.cs
+++ 2025/AdventOfCode/Day4.cs
@@ -1,8 +1,11 @@
 namespace Day4;
 
+record Point(int X, int Y);
+
 class Grid
 {
-    public int Rolls { get; private set; } = 0;
+    public List<Point> Rolls { get; set; } = [];
+    public int TotalRolls { get; set; } = 0;
 
     private char[][] grid;
 
@@ -16,63 +19,91 @@ class Grid
             grid[i] = input[i].ToCharArray();
         }
 
-        for (var x = 0; x < size; x++)
+        do
         {
-            for (var y = 0; y < size; y++)
+            Rolls = [];
+            for (var x = 0; x < size; x++)
             {
-                //Console.WriteLine($"Checking {x} {y}");
-                if (grid[y][x] != '@')
+                for (var y = 0; y < size; y++)
                 {
-                    continue;
-                }
-
-                var tmpRolls = 0;
-
-                for (var k = x - 1; k <= x + 1; k++)
-                {
-                    for (var l = y - 1; l <= y + 1; l++)
+                    if (grid[y][x] != '@')
                     {
-                        if (k < 0 || l < 0 || k >= grid[0].Length || l >= grid.Length)
-                        {
-                            continue;
-                        }
+                        continue;
+                    }
 
-                        if (grid[l][k] == '@')
+                    var tmpRolls = 0;
+
+                    for (var k = x - 1; k <= x + 1; k++)
+                    {
+                        for (var l = y - 1; l <= y + 1; l++)
                         {
-                            tmpRolls++;
+                            if (k < 0 || l < 0 || k >= grid[0].Length || l >= grid.Length)
+                            {
+                                continue;
+                            }
+
+                            if (grid[l][k] == '@')
+                            {
+                                tmpRolls++;
+                            }
                         }
                     }
-                }
-                //Console.WriteLine($"X: {x}, Y: {y}, tmp rolls: {tmpRolls}, rolls {Rolls}");
-
-                if (tmpRolls <= 4)
-                {
-                    Rolls++;
+                    if (tmpRolls <= 4)
+                    {
+                        //Console.WriteLine($"Rolling at {x},{y} with {tmpRolls} adjacent rolls.");
+                        Rolls.Add(new Point(x, y));
+                    }
                 }
             }
-        }
+
+            TotalRolls += Rolls.Count;
+            foreach (var point in Rolls)
+            {
+                grid[point.Y][point.X] = '.';
+            }
+        } while (Rolls.Count() > 0);
     }
 }
 
 public class Day4
 {
-    [Fact]
+    //[Fact]
     public void Test1()
     {
         var lines = AdventOfCode.Utils.ReadInputLines("day4.txt");
         var grid = new Grid();
         grid.Load(lines);
 
-        Assert.Equal(13, grid.Rolls);
+        Assert.Equal(13, grid.Rolls.Count);
     }
 
-    [Fact]
+    //[Fact]
     public void Test2()
     {
         var lines = AdventOfCode.Utils.ReadInputLines("day4b.txt");
         var grid = new Grid();
         grid.Load(lines);
 
-        Assert.Equal(1474, grid.Rolls);
+        Assert.Equal(1474, grid.Rolls.Count);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        var lines = AdventOfCode.Utils.ReadInputLines("day4.txt");
+        var grid = new Grid();
+        grid.Load(lines);
+
+        Assert.Equal(43, grid.TotalRolls);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var lines = AdventOfCode.Utils.ReadInputLines("day4b.txt");
+        var grid = new Grid();
+        grid.Load(lines);
+
+        Assert.Equal(8910, grid.TotalRolls);
     }
 }
